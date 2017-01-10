7bee4bd123778783b9fbbef0454f59a0efa8ed81
Guruparan <cns_guru@yahoo.com>
2017-01-09 22:08:59 -0500

    somewhat working

--- trainTime.js
+++ trainTime.js
@@ -1,21 +1,38 @@
 // Initialize Firebase
 
-var config = {
-  apiKey: "AIzaSyAcMaCtF9wngaAIAtFJAeF7Q2xkzFDjud0",
-  authDomain: "firstfirebasetrinity.firebaseapp.com",
-  databaseURL: "https://firstfirebasetrinity.firebaseio.com",
-  storageBucket: "firstfirebasetrinity.appspot.com",
-  messagingSenderId: "551160138408"
-};
+ var config = {
+    apiKey: "AIzaSyAcMaCtF9wngaAIAtFJAeF7Q2xkzFDjud0",
+    authDomain: "firstfirebasetrinity.firebaseapp.com",
+    databaseURL: "https://firstfirebasetrinity.firebaseio.com",
+    storageBucket: "firstfirebasetrinity.appspot.com",
+    messagingSenderId: "551160138408"
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
 // Create a variable to reference the database
 
 var database = firebase.database();
-// Initial Values
 
+// Initial Values
 var namesAndTimes = [];
+
+var timeTillNextTrain;
+var nextTrainTime;
+
+// var newTrain = {
+//       name : "trainName",
+//       destination : "destination",
+//       frequency : 55,
+//       nextArrival : 22,
+//       minutesAway : 33
+//   };
+
+// namesAndTimes.push(newTrain);
+// console.log("names and trains = " + namesAndTimes);
+// // Save the new train details in Firebase
+// database.ref().set(namesAndTimes);
+
 
 // At the initial load, get a snapshot of the current data.
 database.ref().on("value", function(snapshot) {
@@ -62,44 +79,6 @@ database.ref().on("value", function(snapshot) {
 // If any errors are experienced, log them to console.
 });
 
-// Initialize table of trains
-// $("#tableContentsDiv").empty();
-
-// numberOfTrains = namesAndTimes.length;
-// for (var i = 0; i < numberOfTrains; i ++) {
-
-//   var trainNameDiv = $("<div>");
-//   trainNameDiv.addClass("train-name");
-//   trainNameDiv.addClass("col-sm-3");
-//   trainNameDiv.html("<b>"+namesAndTimes[i].name+"</b>");
-//   $("#tableContentsDiv").append(trainNameDiv);
-
-//   var destinationDiv = $("<div>");
-//   destinationDiv.addClass("columnDestination");
-//   destinationDiv.addClass("col-sm-3");
-//   destinationDiv.html("<b>"+namesAndTimes[i].destination+"</b>");
-//   $("#tableContentsDiv").append(destinationDiv);
-
-//   var frequencyDiv = $("<div>");
-//   frequencyDiv.addClass("columnFrequency");
-//   frequencyDiv.addClass("col-sm-2");
-//   frequencyDiv.html("<b>"+namesAndTimes[i].frequency+"</b>");
-//   $("#tableContentsDiv").append(frequencyDiv);
-
-//   var nextArrivalDiv = $("<div>");
-//   nextArrivalDiv.addClass("columnNextArrival");
-//   nextArrivalDiv.addClass("col-sm-2");
-//   nextArrivalDiv.html("<b>"+namesAndTimes[i].nextArrival+"</b>");
-//   $("#tableContentsDiv").append(nextArrivalDiv);
-
-//   var minutesAwayDiv = $("<div>");
-//   minutesAwayDiv.addClass("columnMinutesAway");
-//   minutesAwayDiv.addClass("col-sm-2");
-//   minutesAwayDiv.html("<b>"+namesAndTimes[i].minutesAway+"</b>");
-//   $("#tableContentsDiv").append(minutesAwayDiv);
-
-// }
-
 // --------------------------------------------------------------
 
 // Whenever a user clicks the submit-bid button
@@ -124,42 +103,92 @@ $("#submit-train-info").on("click", function(event) {
   var newTrain = "train" + lengthOfArray;
   console.log("index of new train = " + newTrain);
 
+
+
+  firstTrainTime = moment(firstTrainTime, "HH:mm");
+  var currentTime = moment();
+  console.log("is first time before current time " + firstTrainTime.isBefore(currentTime));
+
+  if (firstTrainTime.isBefore(currentTime)) {
+
+    var tempMoment = firstTrainTime;
+
+    while (tempMoment.isBefore(currentTime)) {
+      tempMoment = moment(tempMoment).add(frequency, "minutes");
+      console.log("frequency added to first train time");
+      console.log(moment(tempMoment).format("h:mm a"));
+    }
+
+    nextTrainTime = moment(tempMoment).format("h:mm a");
+    currentTime = moment();
+    timeTillNextTrain = moment(tempMoment).diff(currentTime, "minutes");
+    console.log("time till next train = " + timeTillNextTrain);
+
+  } else {
+    // time of first train is after current time
+    timeTillNextTrain = moment(firstTrainMoment).diff(currentTime, "minutes");
+    console.log("time till next train = " + timeTillNextTrain);
+    nextTrainTime = moment(firstTrainTime).format("h:mm a");
+  }
+
   var newTrain = {
       name : trainName,
       destination : destination,
       frequency : frequency,
-      nextArrival : 22,
-      minutesAway : 33
+      nextArrival : nextTrainTime,
+      minutesAway : timeTillNextTrain
   };
 
   namesAndTimes.push(newTrain);
   console.log("names and trains = " + namesAndTimes);
     // Save the new train details in Firebase
-    database.ref().set(namesAndTimes);
+  database.ref().set(namesAndTimes);
 
-    // Log the new train details
-    console.log("New Train info received");
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrainTime);
-    console.log(frequency);
+  // Log the new train details
+  console.log("New Train info received");
+  console.log("trainName = " + trainName);
+  console.log("destination = " + destination);
+  console.log("firstTrainTime = " + firstTrainTime);
+  console.log("frequency = " + frequency);
 
-    // find the time of next train
-    var currentTime = moment().format('LT');
-    console.log(currentTime);
-    console.log("first train time = " + firstTrainTime);
-    var firstTrainTimeFromatted = moment().format('hh:mm', firstTrainTime);
-    console.log("firstTrainTimeFromatted = " + firstTrainTimeFromatted);
-    var timeToNextTrain = firstTrainTime + frequency;
-    console.log("before while " + timeToNextTrain);
-    if (timeToNextTrain <= currentTime) {
-      timeToNextTrain = timeToNextTrain + frequency;
-    }
-    console.log("after while " + timeToNextTrain);
 
-    // temp values for these two variables
-    var nextArrival = 99;
-    var minutesAway = 99; 
+// // find the time of next train
+//   var currentTime = moment();
+//   console.log("currentTime = " + currentTime);
 
-    console.log("latest train info displayed")
-  });
+//   console.log("first train time = " + firstTrainTime);
+//   // firstTrainTime = moment(firstTrainTime, 'HH:mm');
+
+
+//   var firstTrainTime = "08:45";
+//   var frequncy = 90;
+
+//   var currentTime = moment();
+
+//   var firstTrainMoment = moment(firstTrainTime, "HH:mm");
+
+//   console.log(firstTrainMoment.isBefore(currentTime));
+
+
+
+
+
+//   firstTrainTime = moment(firstTrainTime, 'HH:ss');
+//   console.log("firstTrainTime formatted = " + firstTrainTime);
+
+//   var timeToNextTrain = moment(firstTrainTime).add(frequency, 'minutes');
+//   console.log("timeToNextTrain = " + timeToNextTrain);
+
+//   console.log("before while " + timeToNextTrain);
+//   if (timeToNextTrain <= currentTime) {
+//     timeToNextTrain = timeToNextTrain + frequency;
+//   }
+//   console.log("after while " + timeToNextTrain);
+
+//   // temp values for these two variables
+//   var nextArrival = 99;
+//   var minutesAway = 99; 
+
+  console.log("latest train info displayed")
+ 
+});
